8dd251f4cb306ee4dd91da3d8912dddb8213f02e
Maria Monpeat <115364869+mmonpeat@users.noreply.github.com>
2025-08-02 18:17:26 +0200

    Update wordpress.sh

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,1 +1,11 @@
 #! /bin/bash
+
+if [ ! -f wp-config.php ]; then
+	wp core download --allow-root 
+	wp config create --dbname=$DATA_BASE_NAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_USER_PASSWORD --dbhost=$DATA_BASE_HOSTNAME --allow-root
+	wp core install --url=$DOMAIN_NAME --title="$WORDPRESS_TITLE" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
+	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
+	wp theme install twentytwentytwo --activate --allow-root
+fi
+
+/usr/sbin/php-fpm7.4 -F;
