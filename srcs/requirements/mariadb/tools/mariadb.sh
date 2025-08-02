d9c61c7986e5d8850f01d38a0416165c62ffed7b
Maria Monpeat <115364869+mmonpeat@users.noreply.github.com>
2025-08-02 18:19:56 +0200

    Create mariadb.sh

---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+service mariadb start
+
+if [ ! -d /var/lib/mysql/$DATA_BASE_NAME ]; then
+	mysql -u $MARIADB_ROOT_USER -p$MARIADB_ROOT_PASSWORD -e "CREATE DATABASE $DATA_BASE_NAME;"
+	mysql -e "CREATE USER '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
+	mysql -e "GRANT ALL ON $DATA_BASE_NAME.* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD' WITH GRANT OPTION;"
+	mysql -e "FLUSH PRIVILEGES;"
+fi
+
+mysqladmin -u $MARIADB_ROOT_USER --password=$MARIADB_ROOT_PASSWORD shutdown
+
+mysqld
